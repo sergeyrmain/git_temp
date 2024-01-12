4e8c9ac9755a7065dc23e10ad545b24624da6ffe
sergey <sergio44k@gmail.com>
2024-01-12 14:35:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/add', methods=['GET'])
+def add():
+    num1 = request.args.get('num1', default=0, type=int)
+    num2 = request.args.get('num2', default=0, type=int)
+
+    result = num1 + num2
+    return jsonify({"result": result})
+
+
+@app.route('/subtract', methods=['GET'])
+def subtract():
+    num1 = request.args.get('num1', default=0, type=int)
+    num2 = request.args.get('num2', default=0, type=int)
+
+    result = num1 - num2
+    return jsonify({"result": result})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
